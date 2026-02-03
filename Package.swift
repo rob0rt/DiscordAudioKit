d3d5e8e93c06e14a2dca670e19a65231704b2f83
Robert Lynch <robertlynch2012@gmail.com>
2026-02-02 20:37:21 -0800

    update git urls

--- Package.swift
+++ Package.swift
@@ -6,24 +6,21 @@ import PackageDescription
 let package = Package(
     name: "DiscordAudioKit",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DiscordAudioKit",
             targets: ["DiscordAudioKit"]
         ),
     ],
     dependencies: [
-        .package(url: "git@github.com:rob0rt/DaveKit.git", branch: "main"),
-        .package(url: "git@github.com:rob0rt/OpusKit.git", branch: "main"),
-        .package(url: "git@github.com:hummingbird-project/swift-websocket.git", from: "1.3.2"),
-        .package(url: "git@github.com:apple/swift-log.git", from: "1.9.0"),
-        .package(url: "git@github.com:apple/swift-async-algorithms.git", from: "1.1.1"),
+        .package(url: "https://github.com/SwiftDiscordAudio/DaveKit.git", branch: "main"),
+        .package(url: "https://github.com/SwiftDiscordAudio/OpusKit.git", branch: "main"),
+        .package(url: "https://github.com/hummingbird-project/swift-websocket.git", from: "1.3.2"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.9.0"),
+        .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.1.1"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "4.0.0"),
         .package(url: "https://github.com/apple/swift-nio", from: "2.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DiscordAudioKit",
             dependencies: [
